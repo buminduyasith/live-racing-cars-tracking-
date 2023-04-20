62e3af903dd69d8ad77bda18e0bd6dba0acfe362
Chamidu Chamodya <47558333+ChamiduChamodya@users.noreply.github.com>
2023-04-20 20:03:08 +0530

    feat(Track) : MapIntegration

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,19 +1,33 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import Layout from '@/components/layout/layout'
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyCOZnnAX_Bxbn7Gvga8ZUZiXhrFthrdr60'
+  })
+
+
+  if (!isLoaded) return <div>Loading</div>
+
   return (
     <Layout>
       <section className="text-center">
         <div className="container">
           <h1>Live Track Map</h1>
+          <Map />
         </div>
       </section>
     </Layout>
   )
+}
+
+function Map() {
+  return <GoogleMap zoom={19} center={{ lat: 6.0329, lng: 80.2168 }} mapContainerClassName='map-container'>
+    <Marker position={{ lat: 6.0329, lng: 80.2168 }} />
+    <Marker position={{ lat: 6.0429, lng: 80.2268 }} />
+  </GoogleMap>
 }
