6d469d23251469ef8c6198f9889366783d8778a1
Bumindu Yasith <bumiduyasith@gmail.com>
2023-04-22 00:10:13 +0530

    install maptiler

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -4,6 +4,10 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Layout from '@/components/layout/layout'
 
+import Map, { NavigationControl } from 'react-map-gl';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -12,6 +16,17 @@ export default function Home() {
       <section className="text-center">
         <div className="container">
           <h1>Live Track Map</h1>
+          <div>
+            <Map mapLib={maplibregl}
+              initialViewState={{
+                longitude:  5.947824,
+                latitude: 80.5483,
+                zoom: 10
+              }}
+              style={{ width: "100%", height: " calc(100vh - 77px)" }}
+              mapStyle="https://api.maptiler.com/maps/streets/style.json?key=6KlK94OorBZ584iRkFjn"
+            />
+          </div>
         </div>
       </section>
     </Layout>
